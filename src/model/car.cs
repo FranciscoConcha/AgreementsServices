ca847d3694045b5dca434cfd9327a962b3040a09
FranciscoConcha <Francisco.concha.urquieta@gmail.com>
2026-01-14 20:04:36 -0300

    Feat: commit inicial

---
+++ src/model/car.cs
@@ -0,0 +1,10 @@
+namespace src.model.user;
+
+public class CardModel
+{
+    public int Id {get;set;}
+    public string Idpublic {get;set;} ="";
+
+    public int Uses {get;set;}
+
+}   
---
+++ src/model/user.cs
@@ -0,0 +1,8 @@
+namespace src.model.user;
+public class UserModel{
+        public int Id {get;set;}    
+        public string Name{get;set;} = "";
+
+        public string Carrer {get;set;} = "";
+
+    }
